ac4e156d15c678e5ca30963a723949c20d0b966b
Marie Sebert <32982221+mariesstl@users.noreply.github.com>
2020-03-29 21:25:41 -0500

    Add files via upload

--- Lesson 3/recursiveminmax.py
+++ Lesson 3/recursiveminmax.py
@@ -7,14 +7,13 @@ def findMinandMax(numList):
     if len(numList) == 1: #Base Case: Evaluate only one item.
         return min, max #There is only one value. Making it min and max.
     else:
-        newMin,newMax = findMinandMax(numList[1:]) 
+        newMin,newMax = findMinandMax(numList[1:]) #Each call will separate out the left-most value.
         if newMin < min:
             min = newMin
         if newMax > max:
             max = newMax
             
         return min, max 
-
 
             
 print(findMinandMax([1,5,4,2]))
