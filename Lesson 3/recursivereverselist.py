560fed18937eb71dfc9bed9f8983d301d6de337d
Marie Sebert <32982221+mariesstl@users.noreply.github.com>
2020-03-29 22:33:27 -0500

    Update recursivereverselist.py

--- Lesson 3/recursivereverselist.py
+++ Lesson 3/recursivereverselist.py
@@ -5,10 +5,8 @@ def reverseMyList(numList):
     if len(numList) == 1: #Base Case: Evaluate only one item.
         return numList #There is only one value in original list. So, only one value in the new list.
     else:
-        reversedList = reverseMyList(numList[1:])
-        reversedList.append(numList[0])
+        reversedList = reverseMyList(numList[1:])#Separate out the left-most item and then evaluate...all the way down.
+        reversedList.append(numList[0])# append the item to the new list in reversed order.
         return reversedList
-#         reversedList = reversedList[0] + reversedList(numList[1:])
-
             
 print(reverseMyList([5,4,3,2,1]))
